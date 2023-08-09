f27a04f0112dd9ca425dae05b143349497e7b411
atutito <augustoalerino@hotmail.com>
2023-08-08 22:06:43 -0300

    header mejorado

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,9 +5,10 @@ export default function main() {
   return (
     <div className="flex-col-reverse grow sm:flex md:flex lg:flex-row pt-3 items-center gap-3">
           <div className="text-center">
-            <h1 className="max-w-xl mb-4 text-xl font-bold tracking-widest leading-none md:text-2xl xl:text-3xl dark:text-white ">Find the perfect destination</h1>
+            <h1 className="max-w-xl mb-4 text-xl font-bold tracking-tight leading-none md:text-2xl xl:text-3xl dark:text-white ">Find the perfect destination</h1>
             <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Our app will help you find the perfect path for your next trip. With an easy-to-use interface and a host of itinerary options, planning your next trip has never been easier.</p>
-            <Anchor to={'/Cities'} className="inline-flex px-5 py-3 text-base font-medium text-center text-white rounded-lg bg-indigo-600 hover:bg-[#5a4b03] hover:invert cursor-pointer hover:translate-y-1 duration-100">View More</Anchor>
+            <p className='italic font-bold font-bold tracking-wider'>Find your perfect trip, designed by insiders who know and love their cities!</p>
+            <Anchor to={'/Cities'} className="inline-flex px-5 py-3 mt-4 text-base font-medium text-center text-white rounded-lg bg-indigo-600 hover:bg-[#5a4b03] hover:invert cursor-pointer hover:translate-y-1 duration-100">View More</Anchor>
           </div>
           <Carousel />
     </div>   
